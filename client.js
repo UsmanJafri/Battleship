a0cee26b96534b75a1214aef69c550049b239fed
UsmanJafri <usmanjafri@hotmail.com>
2018-06-19 02:43:14 +0500

    added moving aircraft_carrier, added dropdown for ship selection (Step 5)

--- client.js
+++ client.js
@@ -24,17 +24,43 @@ const gridInit = () => {
 }
 
 const shipClick = event => {
-    row = parseInt(event.target.dataset.r)
-    col = parseInt(event.target.dataset.c)
-    state.grid[row].props.children[col] = React.createElement('div',{className: 'box ship-aircraft_carrier',onClick: ev => shipClick(ev),'data-r': r,'data-c': c})
-    setState({grid: state.grid})
+    r = parseInt(event.target.dataset.r)
+    c = parseInt(event.target.dataset.c)
+    startPos = [r,c]
+    if (c < 6 && !state.ac) {
+        for (i = 0;i < 5;i++) {
+            state.grid[r].props.children[c] = React.createElement('div',{className: 'box ship-aircraft_carrier',onClick: ev => shipClick(ev),'data-r': r,'data-c': c})
+            c += 1
+        }
+        setState({grid: state.grid,ac: true,ac_coord: startPos})
+    }
+    else if (c < 6 && state.ac) {
+        oldR = state.ac_coord[0]
+        oldC = state.ac_coord[1]
+        for (i = 0;i < 5;i++) {
+            state.grid[oldR].props.children[oldC] = React.createElement('div',{className: 'box',onClick: ev => shipClick(ev),'data-r': oldR,'data-c': oldC})
+            oldC += 1
+        }
+        for (i = 0;i < 5;i++) {
+            state.grid[r].props.children[c] = React.createElement('div',{className: 'box ship-aircraft_carrier',onClick: ev => shipClick(ev),'data-r': r,'data-c': c})
+            c += 1
+        }
+        setState({grid: state.grid,ac_coord: startPos})
+    }
 }
 
 const setState = updates => {
     Object.assign(state, updates)
     ReactDOM.render(React.createElement('div', null, state.msg,
-        React.createElement('div',null,state.grid)
+        React.createElement('div',null,state.grid),
+        React.createElement('select',null,
+            React.createElement('option',{},'aircraft_carrier'),
+            React.createElement('option',{},'battleship'),
+            React.createElement('option',{},'cruiser'),
+            React.createElement('option',{},'destroyer'),
+            React.createElement('option',{},'submarine')
+        ),
     ),document.getElementById('root'))
 }
 
-setState({msg: 'Hello World',grid: gridInit()})
+setState({msg: 'Hello World',grid: gridInit(),ac: false})
