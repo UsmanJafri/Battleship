198bce6ce8a1620b6b612c9bbec6c9760c8c883b
UsmanJafri <usmanjafri@hotmail.com>
2018-06-18 20:18:13 +0500

    One box of ship rendered (Part 3)

--- client.js
+++ client.js
@@ -11,10 +11,30 @@ const shipsize = {
 }
 const state = {}
 
-const setState = updates => {
-    Object.assign(state, updates)
-    ReactDOM.render(React.createElement('div', null, state.msg), 
-        document.getElementById('root'))
+const gridInit = () => {
+    grid = []
+    for (r = 0;r < 10;r++) {
+        row = []
+        for (c = 0;c < 10;c++) {
+            row.push(React.createElement('div',{className: 'box',onClick: ev => shipClick(ev),'data-r': r,'data-c': c}))
+        }
+        grid.push(React.createElement('div',null,row))
+    }
+    return grid
 }
 
-setState({msg: 'Hello World'})
+const shipClick = event => {
+    row = parseInt(event.target.dataset.r)
+    col = parseInt(event.target.dataset.c)
+    state.grid[row].props.children[col] = React.createElement('div',{className: 'box ship-aircraft_carrier',onClick: ev => shipClick(ev),'data-r': r,'data-c': c})
+    setState({grid: state.grid})
+}
+
+const setState = updates => {
+    Object.assign(state, updates)
+    ReactDOM.render(React.createElement('div', null, state.msg,
+        React.createElement('div',null,state.grid)
+    ),document.getElementById('root'))
+}
+
+setState({msg: 'Hello World',grid: gridInit()})
