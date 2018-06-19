8f1ad0c32f102c855672d779094f3bed80e4ba7e
UsmanJafri <usmanjafri@hotmail.com>
2018-06-19 19:41:40 +0500

    Added guess board (Part 8 complete)

--- client.js
+++ client.js
@@ -11,14 +11,24 @@ const shipsize = {
 }
 const state = {}
 
-const gridInit = () => {
+const gridInit = (isGuess) => {
     grid = []
-    for (r = 0;r < 10;r++) {
-        row = []
-        for (c = 0;c < 10;c++) {
-            row.push(React.createElement('div',{className: 'box',onClick: ev => shipClick(ev),onContextMenu: ev => shipClick(ev),'data-r': r,'data-c': c,'data-occupied': false}))
+    if (isGuess) {
+        for (r = 0;r < 10;r++) {
+            row = []
+            for (c = 0;c < 10;c++) {
+                row.push(React.createElement('div',{className: 'box',onClick: ev => shipClickGuess(ev),onContextMenu: ev => shipClickGuess(ev),'data-r': r,'data-c': c,'data-occupied': false}))
+            }
+            grid.push(React.createElement('div',null,row))
         }
-        grid.push(React.createElement('div',null,row))
+    } else {
+        for (r = 0;r < 10;r++) {
+            row = []
+            for (c = 0;c < 10;c++) {
+                row.push(React.createElement('div',{className: 'box',onClick: ev => shipClick(ev),onContextMenu: ev => shipClick(ev),'data-r': r,'data-c': c,'data-occupied': false}))
+            }
+            grid.push(React.createElement('div',null,row))
+        }
     }
     return grid
 }
@@ -106,6 +116,89 @@ const shipClick = event => {
     }
 }
 
+const shipClickGuess = event => {
+    r = parseInt(event.target.dataset.r)
+    c = parseInt(event.target.dataset.c)
+    startPos = [r,c]
+    ship = state.selectedShip
+    shipSize = parseInt(shipsize[ship])
+    shipClassname = 'box ship-'+ship
+    shipStatus = state[ship+"Guess"]
+    maxDim = 11 - shipSize
+    if (event.type == 'contextmenu') {
+        if (r < maxDim) {
+            for (i = r;i < r + shipSize;i++) {
+                if (state.guessGrid[i].props.children[c].props['className'] === shipClassname) {
+                    continue
+                }
+                else if (state.guessGrid[i].props.children[c].props['data-occupied']) {
+                    return
+                }
+            }
+        }
+        if (r < maxDim && !shipStatus) {
+            for (i = r;i < r + shipSize;i++) {
+                state.guessGrid[i].props.children[c] = React.createElement('div',{className: shipClassname,onClick: ev => shipClickGuess(ev),onContextMenu: ev => shipClickGuess(ev),'data-r': i,'data-c': c,'data-occupied': true})
+            }
+            setState({guessGrid: state.guessGrid,[ship+"Guess"]: true,[ship+"GuessCoord"]: startPos,[ship+"GuessVertical"]: true})
+        }
+        else if (r < maxDim && shipStatus) {
+            oldR = state[ship+"GuessCoord"][0]
+            oldC = state[ship+"GuessCoord"][1]
+            if (state[ship+"GuessVertical"]) {
+                for (i = oldR;i < oldR + shipSize;i++) {
+                    state.guessGrid[i].props.children[oldC] = React.createElement('div',{className: 'box',onClick: ev => shipClickGuess(ev),onContextMenu: ev => shipClickGuess(ev),'data-r': i,'data-c': oldC,'data-occupied': false})
+                }
+            }
+            else {
+                for (i = oldC;i < oldC + shipSize;i++) {
+                    state.guessGrid[oldR].props.children[i] = React.createElement('div',{className: 'box',onClick: ev => shipClickGuess(ev),onContextMenu: ev => shipClickGuess(ev),'data-r': oldR,'data-c': i,'data-occupied': false})
+                }
+            }
+            for (i = r;i < r + shipSize;i++) {
+                state.guessGrid[i].props.children[c] = React.createElement('div',{className: shipClassname,onClick: ev => shipClickGuess(ev),onContextMenu: ev => shipClickGuess(ev),'data-r': i,'data-c': c,'data-occupied': true})
+            }
+            setState({guessGrid: state.guessGrid,[ship+"GuessCoord"]: startPos,[ship+"GuessVertical"]: true})
+        }
+    }
+    else {
+        if (c < maxDim) {
+            for (i = c;i < c + shipSize;i++) {
+                if (state.guessGrid[r].props.children[i].props['className'] === shipClassname) {
+                    continue
+                }
+                else if (state.guessGrid[r].props.children[i].props['data-occupied']) {
+                    return
+                }
+            }
+        }
+        if (c < maxDim && !shipStatus) {
+            for (i = c;i < c + shipSize;i++) {
+                state.guessGrid[r].props.children[i] = React.createElement('div',{className: shipClassname,onClick: ev => shipClickGuess(ev),onContextMenu: ev => shipClickGuess(ev),'data-r': r,'data-c': i,'data-occupied': true})
+            }
+            setState({guessGrid: state.guessGrid,[ship+"Guess"]: true,[ship+"GuessCoord"]: startPos,[ship+"GuessVertical"]: false})
+        }
+        else if (c < maxDim && shipStatus) {
+            oldR = state[ship+"GuessCoord"][0]
+            oldC = state[ship+"GuessCoord"][1]
+            if (state[ship+"GuessVertical"]) {
+                for (i = oldR;i < oldR + shipSize;i++) {
+                    state.guessGrid[i].props.children[oldC] = React.createElement('div',{className: 'box',onClick: ev => shipClickGuess(ev),onContextMenu: ev => shipClickGuess(ev),'data-r': i,'data-c': oldC,'data-occupied': false})
+                }
+            }
+            else {
+                for (i = oldC;i < oldC + shipSize;i++) {
+                    state.guessGrid[oldR].props.children[i] = React.createElement('div',{className: 'box',onClick: ev => shipClickGuess(ev),onContextMenu: ev => shipClickGuess(ev),'data-r': oldR,'data-c': i,'data-occupied': false})
+                }
+            }
+            for (i = c;i < c + shipSize;i++) {
+                state.guessGrid[r].props.children[i] = React.createElement('div',{className: shipClassname,onClick: ev => shipClickGuess(ev),onContextMenu: ev => shipClickGuess(ev),'data-r': r,'data-c': i,'data-occupied': true})
+            }
+            setState({guessGrid: state.guessGrid,[ship+"GuessCoord"]: startPos,[ship+"GuessVertical"]: false})
+        }
+    }
+}
+
 const setState = updates => {
     Object.assign(state, updates)
     ReactDOM.render(React.createElement('div', null, state.msg,
@@ -116,9 +209,9 @@ const setState = updates => {
             React.createElement('option',{},'cruiser'),
             React.createElement('option',{},'destroyer'),
             React.createElement('option',{},'submarine')
-        ),
+        ),React.createElement('div',null,state.guessGrid)
     ),document.getElementById('root'))
-    // console.log(state)
+    console.log(state)
 }
 
-setState({msg: 'Hello World',grid: gridInit(),aircraft_carrier: false,battleship: false,cruiser: false,destroyer: false,submarine: false,selectedShip: 'aircraft_carrier'})
+setState({msg: 'Hello World',grid: gridInit(false),guessGrid: gridInit(true),aircraft_carrier: false,battleship: false,cruiser: false,destroyer: false,submarine: false,selectedShip: 'aircraft_carrier'})
