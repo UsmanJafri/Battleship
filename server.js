6c621606fb6ac33373b5ee1bbb4666b66b5d7778
UsmanJafri <usmanjafri@hotmail.com>
2018-06-23 02:08:04 +0500

    Added server-side verification of ships (Step 9)

--- server.js
+++ server.js
@@ -15,7 +15,41 @@ const io = socketio(server)
 io.sockets.on('connection', socket => {
     console.log('a client connected')
     socket.on('disconnect', () => console.log('a client disconnected'))
-    socket.on('clientState',data => console.log(data))
+    socket.on('clientState',data => {
+        if (!(data.aircraft_carrier && data.battleship && data.cruiser && data.destroyer && data.submarine)) {
+            console.log("Not all ships placed.")
+        } else {
+            ships = ['aircraft_carrier','battleship','cruiser','destroyer','submarine']
+            shipSizes = [5,4,3,2,1]
+            for (i = 0;i < 5;i++) {
+                shipName = 'box ship-' + ships[i]
+                shipCoord = data[ships[i] + 'Coord']
+                let valid = true
+                if (data[ships[i] + 'Vertical']) {
+                    c = shipCoord[1]
+                    for (r = shipCoord[0];r < shipCoord[0] + shipSizes[i];r++) {
+                        if (data.grid[r] === undefined || data.grid[r].props.children[c] === undefined || shipName != data.grid[r].props.children[c].props.className) {
+                            valid = false
+                            break
+                        }
+                    }
+                } else {
+                    r = shipCoord[0]
+                    for (c = shipCoord[1];c < shipCoord[1] + shipSizes[i];c++) {
+                        if (data.grid[r] === undefined || data.grid[r].props.children[c] === undefined || shipName != data.grid[r].props.children[c].props.className) {
+                            valid = false
+                            break
+                        }
+                    }
+                }
+                if (!valid) {
+                    console.log('INVALID:',ships[i],'VERTICAL:',data[ships[i] + 'Vertical'])
+                } else {
+                    console.log(ships[i],'verified.')
+                }
+            }
+        }
+    })
 })
 
 server.listen(8000)
